881f63c174eb8197d946788a0546528f8f603a16
Kayson Yusuph <yusuphkayxn@gmail.com>
2022-02-12 09:44:08 +0300

    Adding staggered widget on posts display on search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -62,7 +63,32 @@ class _SearchScreenState extends State<SearchScreen> {
                 );
               },
             )
-          : const Text('Posts'),
+          : FutureBuilder(
+              future: FirebaseFirestore.instance.collection('posts').get(),
+              builder: (context,
+                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return StaggeredGridView.countBuilder(
+                  crossAxisCount: 3,
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    final postSnap = snapshot.data!.docs[index];
+                    return Image.network(
+                      postSnap['postUrl'],
+                    );
+                  },
+                  staggeredTileBuilder: (index) => StaggeredTile.count(
+                    index % 7 == 0 ? 2 : 1,
+                    index % 7 == 0 ? 2 : 1,
+                  ),
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                );
+              }),
     );
   }
 }
