a54a0e7a90756e5b3642043fa769c9f292057213
Kayson Yusuph <yusuphkayxn@gmail.com>
2022-02-12 09:17:49 +0300

    Modifying search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
@@ -9,14 +10,15 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-
   final TextEditingController _searchContoller = TextEditingController();
+  bool _showUsers = false;
 
   @override
   void dispose() {
     super.dispose();
     _searchContoller.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,10 +28,41 @@ class _SearchScreenState extends State<SearchScreen> {
           controller: _searchContoller,
           decoration: const InputDecoration(hintText: 'Search for a user'),
           onFieldSubmitted: (String val) {
-            print('Value: $val');
+            setState(() {
+              _showUsers = true;
+            });
           },
         ),
       ),
+      body: _showUsers
+          ? FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .where('username',
+                      isGreaterThanOrEqualTo: _searchContoller.text)
+                  .get(),
+              builder: (context,
+                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    final usersnap = snapshot.data!.docs[index].data();
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(usersnap['photoUrl']),
+                      ),
+                      title: Text(usersnap['username']),
+                    );
+                  },
+                );
+              },
+            )
+          : const Text('Posts'),
     );
   }
 }
